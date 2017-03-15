d1cb0450c976a8192bf198322c4a11d1a08987ca
Mark Goho <markgoho@gmail.com>
2017-03-15 17:53:28 -0400

    Replacing api calls with static files

--- src/app/specifications/specifications.component.ts
+++ src/app/specifications/specifications.component.ts
@@ -4,14 +4,6 @@ import { WpEndpoint } from 'ng2-wp-api';
 @Component({
   selector: 'app-specifications',
   styleUrls: ['./specifications.component.scss'],
-  template: `
-    <div class="single-page" [wpModel]="endpoint" [wpId]="10" (wpResponse)="page = $event.data">
-      <h1 [innerHtml]="page?.title?.rendered"></h1>
-      <div [innerHtml]="page?.content?.rendered"></div>
-    </div>
-  `
+  templateUrl: './specifications.component.html'
 })
-export class SpecificationsComponent {
-  endpoint = WpEndpoint.pages;
-
-}
+export class SpecificationsComponent { }
